0248905045f1400caf7bdf68b0931e0bc728e733
SHAYANUL HAQ SADI <shayanul.haq@nexdecade.com>
2024-02-29 14:41:50 +0600

    design done.

--- Calculator/ContentView.swift
+++ Calculator/ContentView.swift
@@ -8,14 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let buttons = [
+        ["AC", "+/-", "%", "/"],
+        ["7", "8", "9", "x"],
+        ["4", "5", "6", "-"],
+        ["1", "2", "3", "+"],
+        ["0", ".", ".", "="]
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack(alignment: .bottom) {
+            Color(.black).edgesIgnoringSafeArea(.all)
+            
+            VStack {
+                HStack {
+                    Spacer()
+                    Text("43")
+                        .font(.system(size: 64))
+                        .foregroundColor(.white)
+                }
+                .padding()
+                
+                ForEach(buttons, id: \.self) { row in
+                    HStack {
+                        ForEach(row, id: \.self ) { button in
+                            Text(button)
+                                .font(.system(size: 32))
+                                .frame(width: 80, height: 80)
+                                .foregroundColor(.white)
+                                .background(.orange)
+                                .cornerRadius(40)
+                        }
+                    }
+                    
+                }
+                
+            }
         }
-        .padding()
     }
 }
 
